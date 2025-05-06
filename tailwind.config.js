911a1505c730487f15269db83df88987dae568ed
nurM7md <nurmohammed931110@gmail.com>
2025-05-06 18:32:05 +0300

    mod img

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,24 @@
 module.exports = {
-  content:[
-    "./*.html" ,
-    "./src/**/*.css" 
-  ],
+  content: ["./*.html", "./src/**/*.css"],
 
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       backgroundImage: {
-        home: "url('https://nurm7md.github.io/Rebuild-Movies-app-using-Tailwindcss-instead-of-regular-CSS./images/photo.jpg')",
+        home: "url('img/home3.jpg')",
       },
       spacing: {
-        '30': '2rem', // 30px = 7.5rem
+        30: "2rem", // 30px = 7.5rem
       },
       lineHeight: {
-        'extra-loose': '4rem',  // تخصيص مسافة إضافية
-        '5xl': '5rem',  // تخصيص مسافة 5rem
-      }
+        "extra-loose": "4rem", // تخصيص مسافة إضافية
+        "5xl": "5rem", // تخصيص مسافة 5rem
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
